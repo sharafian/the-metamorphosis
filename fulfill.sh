530bb7e11382d02e8936fca3d18a860512b8a9ac
Ben Sharafian <benms64@gmail.com>
2017-07-09 14:37:52 +0200

    test: move scripts to test dir

--- fulfill.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X POST 'localhost:8080/rpc?prefix=example.&method=fulfill_condition' -d '["8ef123cb-84cb-0724-153e-f23c1af5e997","rdnt13O8gU7KJxacVIOJKc6dT2j5vqvutk3AWL6I-Js"]'
---
+++ test/unauthorized.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+http POST 'localhost:8080/rpc?prefix=test.fake.&method=send_transfer'
