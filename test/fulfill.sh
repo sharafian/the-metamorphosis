6f53996a87937d04f12afe0f2f87c67b53e11b83
Ben Sharafian <benms64@gmail.com>
2017-07-09 14:37:52 +0200

    test: move scripts to test dir

---
+++ test/fulfill.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST 'localhost:8080/rpc?prefix=test.west.&method=fulfill_condition' -d '["8ef123cb-84cb-0724-153e-f23c1af5e997","rdnt13O8gU7KJxacVIOJKc6dT2j5vqvutk3AWL6I-Js"]' -H 'Authorization: Bearer placeholder'
